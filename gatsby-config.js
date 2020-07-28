0c0a8a23c09ebad845f1f1198b33cfd1d3f21529
Perttu Lähteenlahti <perttu.lahteenlahti@qvik.fi>
2020-07-28 22:33:08 +0300

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,23 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Sleep Apps`,
+    description: `Example of using gatsby-source-app-store to display iOS apps and their reviews on your Gatsby site.`,
+    author: `@plahteenlahti`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-app-store`,
+      options: {
+        country: "fi",
+        apps: [
+          "app.sleepcircle.application",
+          "com.ouraring.oura",
+          "com.aaplab.SleepBot",
+          "com.neybox.Pillow",
+          "com.tantsissa.AutoSleep",
+        ],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -24,11 +37,10 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-styled-components",
   ],
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,160 @@
+import { graphql, PageProps } from "gatsby"
+import React, { FC } from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import styled from "styled-components"
+
+type Review = {
+  id: string
+  text: string
+  score: number
+  title: string
+  url: string
+  version: string
+  userName: string
+  userUrl: string
+}
+
+type App = {
+  appId: string
+  contentRating: string
+  currency: string
+  currentVersionReviews: string
+  currentVersionScore: string
+  description: string
+  developer: string
+  developerId: string
+  developerUrl: string
+  developerWebsite: string
+  free: string
+  genreIds: string
+  genres: string
+  icon: string
+  id: string
+  languages: string
+  price: string
+  primaryGenre: string
+  primaryGenreId: string
+  releaseNotes: string
+  released: string
+  requiredOsVersion: string
+  score: string
+  title: string
+  size: string
+  updated: string
+  url: string
+  version: string
+}
+
+type Props = {
+  allApp: {
+    nodes: Array<App>
+  }
+}
+
+const IndexPage: FC<PageProps<Props>> = ({
+  data: {
+    allApp: { nodes: apps },
+  },
+}) => {
+  console.log(apps)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Gatsby-source-app-store</h1>
+      <p>Display data from apps on App Store on your Website</p>
+
+      <Apps>
+        {apps.map(app => (
+          <Card key={app.id}>
+            <TitleRow>
+              <Icon src={app.icon} />
+              <h5>
+                {app.title} ({app.currentVersionScore})
+              </h5>
+            </TitleRow>
+            <Developer>
+              Developer: <a href={app.developerUrl}>{app.developer}</a>
+            </Developer>
+          </Card>
+        ))}
+      </Apps>
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+const Apps = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin: 0 -0.5rem;
+`
+
+const Card = styled.div`
+  width: calc(50% - 2 * 0.5rem);
+  box-sizing: border-box;
+  border-radius: 0.5rem;
+  margin: 1rem 0.5rem;
+  padding: 1rem;
+  box-shadow: 2px 2px 9px 6px #adadad24;
+`
+
+const Developer = styled.span`
+  font-size: 0.8rem;
+  font-family: sans-serif;
+  a {
+    color: inherit;
+  }
+`
+
+const TitleRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const Icon = styled.img`
+  height: 2rem;
+  width: 2rem;
+  margin-right: 1rem;
+  border-radius: 0.5rem;
+`
+
+export const query = graphql`
+  {
+    allApp {
+      nodes {
+        appId
+        contentRating
+        currency
+        currentVersionReviews
+        currentVersionScore
+        description
+        developer
+        developerId
+        developerUrl
+        developerWebsite
+        free
+        genreIds(difference: "")
+        genres
+        icon
+        id
+        languages
+        price
+        primaryGenre
+        primaryGenreId
+        releaseNotes
+        released
+        requiredOsVersion
+        score
+        title
+        size
+        updated
+        url
+        version
+      }
+    }
+  }
+`
